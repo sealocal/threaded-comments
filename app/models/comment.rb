263ad36615e3e56c26b81502dce65ea062a94931
sealocal <local.mat@gmail.com>
2015-04-30 17:22:56 -0700

    add custom validation for two words in Comment#full_name

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,11 @@
 class Comment < ActiveRecord::Base
   validates :full_name, presence: true
+  validate :full_name_must_contain_at_least_two_tokens
+
+  private
+    def full_name_must_contain_at_least_two_tokens
+      if full_name.blank? || full_name.split.count < 2
+        errors.add(:full_name, "must contain at least two words")
+      end
+    end
 end
