e0b8961ebe03c000e47b2235576141f331d0ca19
sealocal <local.mat@gmail.com>
2015-04-30 21:23:35 -0700

    submit comment#comment_id with form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -75,7 +75,7 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:full_name, :email, :content)
+      params.require(:comment).permit(:full_name, :email, :content, :comment_id)
     end
 
     def authorized?
