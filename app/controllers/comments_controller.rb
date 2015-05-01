cf034ac11486d9579e65effee6834c6ecda54595
sealocal <local.mat@gmail.com>
2015-05-01 09:45:04 -0700

    remove unused javascript

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,11 @@ class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
 
   # GET /comments
-  # GET /comments.json
   def index
     @comments = Comment.where(comment: nil)
   end
 
   # GET /comments/1
-  # GET /comments/1.json
   def show
   end
 
@@ -24,47 +22,34 @@ class CommentsController < ApplicationController
   end
 
   # POST /comments
-  # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
 
-    respond_to do |format|
-      if @comment.save
-        session[:full_name], session[:email] = @comment.full_name, @comment.email
-
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    if @comment.save
+      session[:full_name], session[:email] = @comment.full_name, @comment.email
+      redirect_to @comment, notice: 'Comment was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /comments/1
-  # PATCH/PUT /comments/1.json
   def update
     redirect_to root_path, alert: "You're not authorized!" and return unless authorized?
     respond_to do |format|
       if @comment.update(comment_params)
         format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
       else
         format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /comments/1
-  # DELETE /comments/1.json
   def destroy
     redirect_to root_path, alert: "You're not authorized!" and return unless authorized?
     @comment.destroy
-    respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to comments_url, notice: 'Comment was successfully destroyed.'
   end
 
   private
--- app/views/comments/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @comment, :id, :full_name, :email, :created_at, :updated_at
